099ba2dc628de69e06b0b74487caf8877f41766a
ramsabah <ramsabah28@gmail.com>
2025-07-30 14:35:02 +0200

    apply first colum element

--- lib/src/widgets/Home.dart
+++ lib/src/widgets/Home.dart
@@ -5,14 +5,25 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: const EdgeInsets.only(top: 70),
         width: double.infinity,
         height: double.infinity,
         decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage("lib/assets/hintergruende/bg_mainscreen.png"),
-          fit: BoxFit.cover),
+          image: DecorationImage(
+            image: AssetImage("lib/assets/hintergruende/bg_mainscreen.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+
+          children: [
+            const Text(
+              "Choose Your Favorite Snack",
+              style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
-
     );
   }
 }
